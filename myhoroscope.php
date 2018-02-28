ca91e6cfa40e624454be62fa0220c10e566ecac0
JaoK <jjaokk@gmail.com>
2018-02-28 22:00:19 +0100

    Kolla om cookies finns och hämta informationen i dessa.

--- myhoroscope.php
+++ myhoroscope.php
@@ -1,5 +1,14 @@
 <?php
 
+if(isset($_COOKIE['horoscope'])){	// Våra horoskop-cookies finns
+	// identifiera dessa värden
+	$namn = $_COOKIE['horoscope']['namn'];
+	$efternamn = $_COOKIE['horoscope']['efternamn'];
+	$personnummer = $_COOKIE['horoscope']['personnummer'];
+}
+else{ // Annars skickas tillbaka till index	
+	header('Location: index.php');
+}
 class Person{
 
     function __construct($cookie_name, $cookie_value, $cookie_number) {
